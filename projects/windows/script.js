62346921c62ddc5c217bff8a20503f4fe6f413c9
Jimmy Yuan <TheJimmyYuan@gmail.com>
2014-01-18 22:57:57 -0500

    movingOP

--- projects/windows/script.js
+++ projects/windows/script.js
@@ -1,6 +1,7 @@
 /**
  * @author Jimmy Yuan
  */
+//START MENU
 var menu = "false";
 function startMenu()
 {
@@ -15,6 +16,7 @@ function startMenu()
 	}
 	
 }
+
 function closeSystem()
 {
 	if(menu == "true")
@@ -24,13 +26,8 @@ function closeSystem()
 	}
 	
 }
-function closeApp(input,inputdos)
-{
-	app = input;
-	icon = inputdos;
-	document.getElementById(app).style.display ="none";
-	document.getElementById(icon).style.boxShadow = "";
-}
+//--------------------------------------------------------------------------------------------------------------------------------
+//DAY
 function getTheDay()
 {
 	date = new Date();
@@ -40,12 +37,20 @@ function getTheDay()
 	document.getElementById("date").innerHTML = month + "/" + day + "/" + year;
 }
 
-
-
+//--------------------------------------------------------------------------------------------------------------------------------
+//APPS
 function openApp(app,icon)
 {
 	document.getElementById(app).style.display = "block";
 	document.getElementById(icon).style.boxShadow = "0px 10px 20px 0px #FFF inset";	
+}
+
+function closeApp(input,inputdos)
+{
+	app = input;
+	icon = inputdos;
+	document.getElementById(app).style.display ="none";
+	document.getElementById(icon).style.boxShadow = "";
 }
 
 function go()
@@ -54,16 +59,36 @@ function go()
 	document.getElementById("frame").src = address;
 }
 
-
-
+//FUNCTIONALITY
+//--------------------------------------------------------------------------------------------------------------------------------
 var move = "";
 function drag(input,e)
 {
-	
+    move = document.getElementById(input);
+    document.addEventListener ("mousemove" , mouseMove , false);
+}
+
+function mouseMove(e) {
+    x = e.screenX;
+    y = e.screenY;
+   
+   	//alert("x:" + x + " & " + "y:" + y);
+    
+    move.style.left = (x - x/2)  + "px";
+    move.style.top = (y - 100) + "px";
+    document.addEventListener ("mouseup" , mouseUp , false);
+}
+function mouseUp () {
+    document.removeEventListener ("mousemove" , mouseMove , false);
+    document.removeEventListener ("mouseup" , mouseUp , false);
+}
+/*
+var move = "";
+function drag(input,e)
+{
 	target = input;
 	move = document.getElementById(target);
 	move.addEventListener ("mousedown" , mouseDown , false);
-	
 }
 function mouseDown () {
     stateMouseDown = true;
@@ -76,8 +101,7 @@ function mouseMove (ev) {
     move.style.top = pY + "px";
     document.addEventListener ("mouseup" , mouseUp , false);
 }
-
 function mouseUp () {
     document.removeEventListener ("mousemove" , mouseMove , false);
     document.removeEventListener ("mouseup" , mouseUp , false);
-}
+}*/
