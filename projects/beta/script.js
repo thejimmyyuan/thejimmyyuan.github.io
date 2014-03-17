b913a0fc0eb605493ee1e483479fb43552de1ac2
Jimmy Yuan <TheJimmyYuan@gmail.com>
2014-03-17 14:34:28 -0400

    fuckchrome

--- projects/beta/script.js
+++ projects/beta/script.js
@@ -1,57 +1,21 @@
 /**
  * @author Jimmy Yuan
  */
-var loaded = "false";
-function loadMenu()
+function aboutAni()
 {
-	if(loaded == "true")
-	{
-		loaded = "false";
-		document.getElementById("shadow").style.opacity = "0";
-		document.getElementById("hidnav").style.display = "none";
-	}
-	else{
-	loaded = "true";
-	document.getElementById("shadow").style.background = "black";
-	document.getElementById("shadow").style.opacity = ".5";
-	document.getElementById("hidnav").style.display = "block";
-	}
-}
-function themeMenu()
-{
-	if(loaded == "true")
-	{
-		loaded = "false";
-		document.getElementById("shadow").style.opacity = "0";
-		document.getElementById("themeChoose").style.display = "none";
-	}
-	else{
-	loaded = "true";
-	document.getElementById("shadow").style.background = "black";
-	document.getElementById("shadow").style.opacity = ".5";
-	document.getElementById("themeChoose").style.display = "block";
-	}
-}
-function loadBox(input)
-{
-	box = input;
-	document.getElementById("professional").style.display = "none";
-	document.getElementById("about").style.display = "none";
+	//document.getElementById("pro").style.opacity = ".1";
+	document.getElementById("pro").style.display = "none";
+	//document.getElementById("casual").style.opacity = ".1";
 	document.getElementById("casual").style.display = "none";
-	document.getElementById(box).style.display = "block";
-	loadMenu();
+	document.getElementById("about").style.display= "none";
+	document.getElementById("aboutMe").style.display = "block";
+	document.getElementById("aboutMe").style.opacity = "1";
 }
-function changeTheme(input)
+function back()
 {
-	color = input;
-	if(color == 'blue')
-	{
-		document.getElementById("body").style.background = "cornflowerblue";
-		document.getElementById("body").style.color = "white";
-	}
-	else{
-		document.getElementById("body").style.background = "black";
-		document.getElementById("body").style.color = "white";
-	}
-	themeMenu();
+	document.getElementById("pro").style.display = "inline";
+	document.getElementById("casual").style.display = "inline";
+	document.getElementById("about").style.display= "inline";
+	document.getElementById("aboutMe").style.display = "none";
+	
 }
