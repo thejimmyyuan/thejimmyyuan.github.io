a7f867eb80903b60ab978701580a7d339df98929
Jimmy Yuan <Jimmy@adm-129-49-125-70.wi-fi.stonybrook.edu>
2014-08-28 10:45:26 -0400

    hr

--- framework/script.js
+++ framework/script.js
@@ -1,6 +1,6 @@
 //Jimmy Yuan
 
-function openDiv(name,obj) {
+function openDiv(name) {
 	
 		document.getElementById("about").style.display = "none";
 		document.getElementById("thoughts").style.display = "none";
@@ -8,6 +8,5 @@ function openDiv(name,obj) {
 		document.getElementById("casual").style.display = "none";
 		document.getElementById("projects").style.display = "none";
 		document.getElementById(name).style.display = "block";
-		obj.setAttribute("aria-pressed, true");
 
 }
