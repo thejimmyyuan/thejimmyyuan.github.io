e29985795d08be37a06d58230125f236d3e699d3
Jimmy Yuan <Jimmy@adm-129-49-124-152.wi-fi.stonybrook.edu>
2014-09-15 19:59:25 -0400

    ohgod

--- framework/script.js
+++ framework/script.js
@@ -5,6 +5,7 @@ function openDiv(name) {
 		document.getElementById("about").style.display = "none";
 		document.getElementById("thoughts").style.display = "none";
 		document.getElementById("pro").style.display = "none";
+		document.getElementById("acad").style.display = "none";
 		document.getElementById("casual").style.display = "none";
 		document.getElementById("projects").style.display = "none";
 		document.getElementById(name).style.display = "block";
