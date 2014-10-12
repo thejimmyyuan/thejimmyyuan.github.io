320e974256aeadcc1f7a10ad9b25b2850727198b
Jimmy Yuan <Jimmy@Jimmys-MacBook-Pro.local>
2014-10-11 20:07:27 -0400

    wif

--- projects/Windoges8/framework/script.js
+++ projects/Windoges8/framework/script.js
@@ -2,34 +2,41 @@
  * @author Jimmy Yuan
  */
 //START MENU
-var menu = "false";
-function startMenu()
-{
-	if(menu == "true")
-	{
-		menu = "false";
-		document.getElementById("menu").style.display = "none";
+//--------------------------------------------------------------------------------------------------------------------------------
+//Swap Icons
+swap = false;
+function swapIcon(imgid, stateOne, stateTwo) {
+	if (swap) {
+		document.getElementById(imgid).src = "framework/images/" + stateOne;
+		swap = false;
+	} else {
+		document.getElementById(imgid).src = "framework/images/" + stateTwo;
+		swap = true;
 	}
-	else{
-	menu = "true";
-	document.getElementById("menu").style.display = "block";
-	}
-	
+
 }
 
-function closeSystem()
-{
-	if(menu == "true")
-	{
-		menu = "false";
-		document.getElementById("menu").style.display = "none";
-	}
+//--------------------------------------------------------------------------------------------------------------------------------
+//Sliding
+var menu = false;
+function slideLeft(el) {
 	
+	menu = true;
+	document.getElementById(el).style.right = "0px";
+
 }
+
+function closeSystem(el) {
+	
+	if (menu) {
+		menu = false;
+		document.getElementById(el).style.right = "-18em";
+	}
+}
+
 //--------------------------------------------------------------------------------------------------------------------------------
 //DAY
-function getTheDay()
-{
+function getTheDay() {
 	date = new Date();
 	month = date.getMonth() + 1;
 	day = date.getDate();
@@ -39,37 +46,31 @@ function getTheDay()
 
 //--------------------------------------------------------------------------------------------------------------------------------
 //APPS
-function openApp(app,icon)
-{
+function openApp(app, icon) {
 	document.getElementById(app).style.display = "block";
-	if(icon === "reIcon")
-	{
-		document.getElementById(icon).style.display = "block";	
-		document.getElementById(icon).style.boxShadow = "0px 10px 20px 0px #FFF inset";	
+	if (icon === "reIcon") {
+		document.getElementById(icon).style.display = "block";
+		document.getElementById(icon).style.boxShadow = "0px 10px 20px 0px #FFF inset";
+		closeSystem();
+	} else {
+		document.getElementById(icon).style.boxShadow = "0px 10px 20px 0px #FFF inset";
 		closeSystem();
 	}
-	else{
-		document.getElementById(icon).style.boxShadow = "0px 10px 20px 0px #FFF inset";	
-		closeSystem();
-	}
-	
+
 }
 
-function closeApp(input,inputdos)
-{
+function closeApp(input, inputdos) {
 	app = input;
 	icon = inputdos;
-	document.getElementById(app).style.display ="none";
+	document.getElementById(app).style.display = "none";
 	document.getElementById(icon).style.boxShadow = "";
-	if(icon === "reIcon")
-	{
-		document.getElementById(icon).style.display ="none";
+	if (icon === "reIcon") {
+		document.getElementById(icon).style.display = "none";
 	}
-	
+
 }
 
-function go()
-{
+function go() {
 	address = document.getElementById("bar").value;
 	document.getElementById("frame").src = address;
 }
@@ -80,96 +81,63 @@ var move = "";
 var offX;
 var offY;
 //THANK YOU STACK OVERFLOW
-function drag(input,e)
-{
+function drag(input, e) {
 	offX = e.clientX - parseInt(move.offsetLeft);
-    offY = e.clientY - parseInt(move.offsetTop);
-    move = document.getElementById(input);
-    document.addEventListener ("mousemove" , mouseMove , false);
+	offY = e.clientY - parseInt(move.offsetTop);
+	move = document.getElementById(input);
+	document.addEventListener("mousemove", mouseMove, false);
 }
 
 function mouseMove(e) {
 
-    x = e.screenX;
-    y = e.screenY;
-   
-   	//alert("x:" + x + " & " + "y:" + y);
-    
-    move.style.left = x-offX  + "px";
-    move.style.top = y-offY - 100 + "px";
-    document.addEventListener ("mouseup" , mouseUp , false);
+	x = e.screenX;
+	y = e.screenY;
+
+	//alert("x:" + x + " & " + "y:" + y);
+
+	move.style.left = x - offX + "px";
+	move.style.top = y - offY - 100 + "px";
+	document.addEventListener("mouseup", mouseUp, false);
 }
-function mouseUp () {
-    document.removeEventListener ("mousemove" , mouseMove , false);
-    document.removeEventListener ("mouseup" , mouseUp , false);
+
+function mouseUp() {
+	document.removeEventListener("mousemove", mouseMove, false);
+	document.removeEventListener("mouseup", mouseUp, false);
 }
+
 //CHANGE BG
 //--------------------------------------------------------------------------------------------------------------------------------
-function changebg(input)
-{
-	document.getElementById("desktop").style.background="url(images/wallpapers/" + input + ") no-repeat center center fixed";
+function changebg(input) {
+	document.getElementById("desktop").style.background = "url(images/wallpapers/" + input + ") no-repeat center center fixed";
 }
+
 //Open Link
 //--------------------------------------------------------------------------------------------------------------------------------
-function openLink(input)
-{
-	var win=window.open(input, '_blank');
-    win.focus();
+function openLink(input) {
+	var win = window.open(input, '_blank');
+	win.focus();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
-var move = "";
-function drag(input,e)
-{
-	target = input;
-	move = document.getElementById(target);
-	move.addEventListener ("mousedown" , mouseDown , false);
-}
-function mouseDown () {
-    stateMouseDown = true;
-    document.addEventListener ("mousemove" , mouseMove , false);
-}
-function mouseMove (ev) {
-    var pX = ev.pageX;
-    var pY = ev.pageY;
-    move.style.left = pX + "px";
-    move.style.top = pY + "px";
-    document.addEventListener ("mouseup" , mouseUp , false);
-}
-function mouseUp () {
-    document.removeEventListener ("mousemove" , mouseMove , false);
-    document.removeEventListener ("mouseup" , mouseUp , false);
-}*/
+ var move = "";
+ function drag(input,e)
+ {
+ target = input;
+ move = document.getElementById(target);
+ move.addEventListener ("mousedown" , mouseDown , false);
+ }
+ function mouseDown () {
+ stateMouseDown = true;
+ document.addEventListener ("mousemove" , mouseMove , false);
+ }
+ function mouseMove (ev) {
+ var pX = ev.pageX;
+ var pY = ev.pageY;
+ move.style.left = pX + "px";
+ move.style.top = pY + "px";
+ document.addEventListener ("mouseup" , mouseUp , false);
+ }
+ function mouseUp () {
+ document.removeEventListener ("mousemove" , mouseMove , false);
+ document.removeEventListener ("mouseup" , mouseUp , false);
+ }*/
