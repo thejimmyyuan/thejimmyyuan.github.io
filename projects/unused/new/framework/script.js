04f411c62d585c36c05f1ee467eec757fe4c4c97
Jimmy Yuan <Jimmy@Jimmys-MacBook-Pro.local>
2014-12-30 00:59:52 -0500

    v3

---
+++ projects/unused/new/framework/script.js
@@ -0,0 +1,13 @@
+//Jimmy Yuan
+
+function openDiv(name) {
+	
+		document.getElementById("about").style.display = "none";
+		document.getElementById("thoughts").style.display = "none";
+		document.getElementById("pro").style.display = "none";
+		document.getElementById("acad").style.display = "none";
+		document.getElementById("casual").style.display = "none";
+		document.getElementById("projects").style.display = "none";
+		document.getElementById(name).style.display = "block";
+
+}
