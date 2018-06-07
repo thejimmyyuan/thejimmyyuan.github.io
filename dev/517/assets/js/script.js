658cdda336a8a9cac5ed316a4eae07d38b049e48
Jimmy Yuan <TheJimmyYuan@gmail.com>
2018-06-06 22:24:33 -0400

    updated resume

---
+++ dev/517/assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage();
+});
+$(function(){
+    $("#slogan").typed({
+      strings: ["Front End Developer, ^1000Photographer, ^1000Traveler"],
+      typeSpeed: 0,
+      showCursor: false
+    });
+});
